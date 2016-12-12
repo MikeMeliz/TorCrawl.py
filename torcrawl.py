ce124b68b9c38b427b0168a7acfc1c86147011ee
Mike <mikemel@hotmail.gr>
2016-12-12 20:52:48 +0200

    Remove unnecessary comments

--- torcrawl.py
+++ torcrawl.py
@@ -84,26 +84,7 @@ def checkIP():
     except:
       e = sys.exc_info()[0]
       print( "Error: %s" % e + "\n## IP can't obtain \n## Is " + webIPcheck + "up?")
-'''
-# Write output to file
-def extractorFile(outputFile, website):
-    try:
-      f = open(outputFile,'w')
-      f.write(urllib2.urlopen(website).read())
-      f.close()
-      print '## File created on ' + os.getcwd() + '/' + outputFile
-    except:
-      e = sys.exc_info()[0]
-      print("Error: %s" % e + "\n## Not valid URL \n## Did you forget \'http://\'?")
 
-# Write output to terminal
-def extractor(website):
-    try:
-      print urllib2.urlopen(website).read()
-    except:
-      e = sys.exc_info()[0]
-      print("Error: %s" % e + "\n## Not valid URL \n## Did you forget \'http://\'?")
-'''
 
 def main(argv):
     verbose = False

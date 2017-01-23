6e7335ee2bba9361c53aeeda6dd85d9273ae4f69
Mike <mikemel@hotmail.gr>
2017-01-23 14:17:19 +0200

    Upgrade from getopt to argparse

--- torcrawl.py
+++ torcrawl.py
@@ -40,14 +40,17 @@ License: GNU General Public License v3.0
 
 import sys
 import os
-import getopt
 import socket
 import socks
+import argparse
 
 # TorCrawl Modules
 from modules.crawler import crawler
 from modules.extractor import extractor
 from modules.checker import *
+
+# TODO[G]: Make GUI with Gooey after impl/ion of argparse
+#from gooey import Gooey, GooeyParser
 
 # Set socket and connection with TOR network
 def connectTor():
@@ -64,75 +67,67 @@ def connectTor():
       e = sys.exc_info()[0]
       print( "Error: %s" % e +"\n## Can't establish connection with TOR")
 
-
+# TODO[G]: @Gooey
 def main(argv):
-    extract = True
-    extractarg, verbose, withoutTor, outputToFile, crawl, logs = [False] * 6
-    cdepth = simultaneous = 1
+    # Initialize nececery variables
+    inputFile = outputFile = ''
     cpause = 0
-    website = outputFile = inputFile = outpath = ''
+    cdepth = 1
 
-    try:
-      opts, args = getopt.getopt(argv,'hvu:wei:o:cd:p:l',["help","verbose","url=","without","extract","input=","output=","crawl","depth=","pause=","log"])
-    except getopt.GetoptError:
-      print('usage: torcrawl.py [options]')
-      sys.exit(2)
-    for opt, arg in opts:
+    # Get arguments with argparse
+    parser = argparse.ArgumentParser(description="TorCrawl.py is a python script to crawl and extract (regular or onion) webpages through TOR network.")
+    # General
+    parser.add_argument('-v','--verbose', action='store_true', help='Show more informations about the progress')
+    parser.add_argument('-u','--url', required=True, help='URL of Webpage to crawl or extract')
+    parser.add_argument('-w','--without', action='store_true', help='Without the use of Relay TOR')
+    # Extract
+    parser.add_argument('-e','--extract', action='store_true', help='Extract page\'s code to terminal or file.')
+    parser.add_argument('-i','--input', help='Input file with URL(s) (seperated by line)')
+    parser.add_argument('-o','--output', help='Output page(s) to file(s) (for one page)')
+    # Crawl
+    parser.add_argument('-c','--crawl', action='store_true', help='Crawl website (Default output on /links.txt)')
+    parser.add_argument('-d','--cdepth', help='Set depth of crawl\'s travel (Default: 1)')
+    #parser.add_argument('-z','--exclusions' help='Paths that you don\'t want to include')
+    #parser.add_argument('-s','--simultaneous' help='How many pages to visit at the same time')
+    parser.add_argument('-p','--pause', help='The length of time the crawler will pause')
+    parser.add_argument('-l','--log', action='store_true', help='A save log will let you see which URLs were visited')
 
-      # General
-      if opt in ("-h", "--help"):
-        print help
-        sys.exit()
-      elif opt in ("-v", "--verbose"):
-        verbose = True
-      elif opt in ("-u", "--url"):
-        website = arg
-      elif opt in ("-w", "--without"):
-        withoutTor = True
+    args = parser.parse_args()
 
-      # Extract
-      elif opt in ("-e", "--extract"):
-        extractarg = True
-      elif opt in ("-i", "--input"):
-        inputFile = arg
-      elif opt in ("-o", "--output"):
-        outputFile = arg
+    # Parse arguments to variables
+    if args.input:
+      inputFile = args.input
+    if args.output:
+      outputFile = args.output
+    if args.cdepth:
+      cdepth = args.cdepth
 
-      # Crawl
-      elif opt in ("-c", "--crawl"):
-        crawl = True
-      elif opt in ("-d", "--cdepth"):
-        cdepth = arg
-      elif opt in ("-p", "--pause"):
-        cpause = arg
-      elif opt in ("-l", "--log"):
-        logs = True
-        
     # Connect to TOR
-    if withoutTor == False:
-      checkTor(verbose)
+    if (args.without is False):
+      checkTor(args.verbose)
       connectTor()
     
-    if verbose == True:
+    if args.verbose:
       checkIP()
-      print('## URL: ' + website)
+      print('## URL: ' + args.url)
 
     # Canon/ion of website and create path for output 
-    if len(website) > 0:
-      website = urlcanon(website, verbose)
-      outpath = folder(website, verbose)
+    if len(args.url) > 0:
+      website = urlcanon(args.url, args.verbose)
+      outpath = folder(website, args.verbose)
 
-    if crawl == True:
-      lst = crawler(website, cdepth, cpause, outpath, logs, verbose)
+    if args.crawl == True:
+      lst = crawler(website, cdepth, args.pause, outpath, args.log, args.verbose)
       lstfile = open(outpath + '/links.txt', 'w+')
       for item in lst:
         lstfile.write("%s\n" % item)
       lstfile.close()
       print("## File created on " + os.getcwd() + "/" + outpath + "/links.txt")
-      if extractarg == True:
+      if args.extract == True:
         inputFile = outpath + "/links.txt"
-        extractor(website, crawl, outputFile, inputFile, outpath, verbose)
+        extractor(args.url, args.crawl, outputFile, inputFile, outpath, args.verbose)
     else: 
-      extractor(website, crawl, outputFile, inputFile, outpath, verbose)
+      extractor(args.url, args.crawl, outputFile, inputFile, outpath, args.verbose)
+
 if __name__ == "__main__":
     main(sys.argv[1:])

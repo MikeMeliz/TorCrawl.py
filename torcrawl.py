992495b71b5c48668714ac499b214e25750f1d7d
MikeMeliz <mikemel@hotmail.gr>
2020-08-02 18:46:44 +0300

    PEP8PysockDepend
    Signed-off-by: MikeMeliz <mikemel@hotmail.gr>

--- torcrawl.py
+++ torcrawl.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python
 
+import socket
+import socks
+import argparse
+
+# TorCrawl Modules
+from modules.crawler import crawler
+from modules.extractor import extractor
+from modules.checker import *
+
 help = '''
 
 TorCrawl.py is a python script to crawl and extract (regular or onion) 
@@ -9,7 +18,7 @@ usage: python torcrawl.py [options]
 python torcrawl.py -u l0r3m1p5umD0lorS1t4m3t.onion 
 python torcrawl.py -v -w -u http://www.github.com -o github.htm 
 python torcrawl.py -v -u l0r3m1p5umD0lorS1t4m3t.onion -c -d 2 -p 5
-python torcrawl.py -v -w -u http://www.github.com -c -d 2 -p 5 -e -o GitHub
+python torcrawl.py -v -w -u http://www.github.com -c -d 2 -p 5 -e -f GitHub
 
 General:
 -h, --help         : Help
@@ -32,26 +41,16 @@ Crawl:
                     (Default: 0)
 -f, --folder	  : The root directory which will contain the 
                     generated files
--l, --log         : A save log will let you see which URLs were
-                    visited (TODO)
+-l, --log         : Log file with visited URLs and their response code.
 
 GitHub: github.com/MikeMeliz/TorCrawl.py
 License: GNU General Public License v3.0
 
 '''
 
-import socket
-import socks
-import argparse
-
-# TorCrawl Modules
-from modules.crawler import crawler
-from modules.extractor import extractor
-from modules.checker import *
-
 
 # Set socket and connection with TOR network
-def connectTor():
+def connecttor():
 	try:
 		port = 9050
 		# Set socks proxy and wrap the urllib module
@@ -65,12 +64,12 @@ def connectTor():
 		socket.getaddrinfo = getaddrinfo
 	except:
 		e = sys.exc_info()[0]
-		print("Error: %s" % e + "\n## Can't establish connection with TOR")
+		print(("Error: %s" % e + "\n## Can't establish connection with TOR"))
 
 
 def main():
 	# Initialize necessary variables
-	inputFile = outputFile = ''
+	inputfile = outputfile = ''
 	cpause = 0
 	cdepth = 1
 
@@ -79,57 +78,79 @@ def main():
 		description="TorCrawl.py is a python script to crawl and extract (regular or onion) webpages through TOR network.")
 
 	# General
-	parser.add_argument('-v',
-						'--verbose',
-						action='store_true',
-						help='Show more informations about the progress')
-	parser.add_argument('-u',
-						'--url',
-						required=True,
-						help='URL of Webpage to crawl or extract')
-	parser.add_argument('-w',
-						'--without',
-						action='store_true',
-						help='Without the use of Relay TOR')
+	parser.add_argument(
+		'-v',
+		'--verbose',
+		action='store_true',
+		help='Show more information about the progress'
+	)
+	parser.add_argument(
+		'-u',
+		'--url',
+		required=True,
+		help='URL of webpage to crawl or extract'
+	)
+	parser.add_argument(
+		'-w',
+		'--without',
+		action='store_true',
+		help='Without the use of Relay TOR'
+	)
 
 	# Extract
-	parser.add_argument('-e',
-						'--extract',
-						action='store_true',
-						help='Extract page\'s code to terminal or file.')
-	parser.add_argument('-i',
-						'--input',
-						help='Input file with URL(s) (seperated by line)')
-	parser.add_argument('-o',
-						'--output',
-						help='Output page(s) to file(s) (for one page)')
+	parser.add_argument(
+		'-e',
+		'--extract',
+		action='store_true',
+		help='Extract page\'s code to terminal or file.'
+	)
+	parser.add_argument(
+		'-i',
+		'--input',
+		help='Input file with URL(s) (seperated by line)'
+	)
+	parser.add_argument(
+		'-o',
+		'--output',
+		help='Output page(s) to file(s) (for one page)'
+	)
 
 	# Crawl
-	parser.add_argument('-c',
-						'--crawl',
-						action='store_true',
-						help='Crawl website (Default output on /links.txt)')
-	parser.add_argument('-d',
-						'--cdepth',
-						help='Set depth of crawl\'s travel (Default: 1)')
-	parser.add_argument('-p',
-						'--pause',
-						help='The length of time the crawler will pause')
-	parser.add_argument('-l',
-						'--log',
-						action='store_true',
-						help='A save log will let you see which URLs were visited')
-	parser.add_argument('-f',
-						'--folder',
-						help='The root directory which will contain the generated files')
+	parser.add_argument(
+		'-c',
+		'--crawl',
+		action='store_true',
+		help='Crawl website (Default output on /links.txt)'
+	)
+	parser.add_argument(
+		'-d',
+		'--cdepth',
+		help='Set depth of crawl\'s travel (Default: 1)'
+	)
+	parser.add_argument(
+		'-p',
+		'--pause',
+		help='The length of time the crawler will pause'
+	)
+	parser.add_argument(
+		'-l',
+		'--log',
+		action='store_true',
+		help='A save log will let you see which URLs were visited and their response code'
+	)
+	parser.add_argument(
+		'-f',
+		'--folder',
+		help='The root directory which will contain the generated files'
+	)
 
 	args = parser.parse_args()
 
 	# Parse arguments to variables
 	if args.input:
-		inputFile = args.input
+		inputfile = args.input
 	if args.output:
-		outputFile = args.output
+		outputfile = args.output
 	if args.cdepth:
 		cdepth = args.cdepth
 	if args.pause:
@@ -137,12 +158,12 @@ def main():
 
 	# Connect to TOR
 	if args.without is False:
-		checkTor(args.verbose)
-		connectTor()
+		checktor(args.verbose)
+		connecttor()
 
 	if args.verbose:
-		checkIP()
-		print('## URL: ' + args.url)
+		checkip()
+		print(('## URL: ' + args.url))
 
 	# Canon/ion of website and create path for output
 	if len(args.url) > 0:
@@ -152,7 +173,7 @@ def main():
 		if args.folder is not None:
 			outpath = folder(args.folder, args.verbose)
 		else:
-			outpath = folder(website, args.verbose)
+			outpath = folder(extract_domain(website), args.verbose)
 
 	if args.crawl:
 		lst = crawler(website, cdepth, cpause, outpath, args.log, args.verbose)
@@ -160,12 +181,12 @@ def main():
 		for item in lst:
 			lstfile.write("%s\n" % item)
 		lstfile.close()
-		print("## File created on " + os.getcwd() + "/" + outpath + "/links.txt")
+		print(("## File created on " + os.getcwd() + "/" + outpath + "/links.txt"))
 		if args.extract:
-			inputFile = outpath + "/links.txt"
-			extractor(website, args.crawl, outputFile, inputFile, outpath)
+			inputfile = outpath + "/links.txt"
+			extractor(website, args.crawl, outputfile, inputfile, outpath)
 	else:
-		extractor(website, args.crawl, outputFile, inputFile, outpath)
+		extractor(website, args.crawl, outputfile, inputfile, outpath)
 
 
 if __name__ == "__main__":
